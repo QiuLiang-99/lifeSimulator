47eee96dee8c93b3fc7bccd7ab8c3e0fdc930fd9
QiuLiang-99 <1297829693@qq.com>
2024-04-30 17:53:38 +0800

    Singleton

--- src/scene/scene.h
+++ src/scene/scene.h
@@ -7,7 +7,23 @@
 #include <QLabel>
 #include <QWidget>
 #include <qlayout.h>
+// 单例模板
+template <typename T>
+class Singleton {
+  public:
+    static T& GetInstance() {
+      static T instance;
+      return instance;
+    }
 
+    Singleton(T&&)           = delete;
+    Singleton(const T&)      = delete;
+    void operator=(const T&) = delete;
+
+  protected:
+    Singleton()          = default;
+    virtual ~Singleton() = default;
+};
 class Scene : public QWidget {
     Q_OBJECT
 
